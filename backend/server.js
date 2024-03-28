01c48f1eeb1b7d80c801b4a80fa0a05196967638
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-03-27 22:39:51 -0500

    set up userSchema

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,16 @@ const cors = require("cors");
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
 const PORT = process.env.PORT || 5000;
 
 // Connect to DB and start server
