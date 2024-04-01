58998b78ce3f9c065caa66e9cea9e1f1f68e41f3
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-03-31 23:32:01 -0500

    set up user registration and add validation

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
 
 const app = express();
 
@@ -11,12 +13,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Routes Middleware
+app.use("/api/users", userRoute);
+
 // Routes
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
 const PORT = process.env.PORT || 5000;
+
+// Error Middleware
+app.use(errorHandler);
 
 // Connect to DB and start server
 mongoose
