2b4822210fc78f0efb87f7bcaa7ecda3b1923e30
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-03-31 23:56:15 -0500

    add encryption to user registration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -20,7 +21,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Please add a password"],
       minLength: [6, "Password must be at least 6 characters in length"],
-      maxLength: [23, "Password must not be more than 23 characters in length"],
+      // maxLength: [23, "Password must not be more than 23 characters in length"],
     },
     photo: {
       type: String,
@@ -41,6 +42,18 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Encrypt password before saving to DB
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
