767a51514eb47b0bd8d35096529bc1b966def178
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-04-02 12:32:06 -0500

    create getUser route and getUser controller with authentication

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,11 +3,14 @@ const {
   registerUser,
   loginUser,
   logout,
+  getUser,
 } = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
+router.get("/getUser", protect, getUser);
 
 module.exports = router;
