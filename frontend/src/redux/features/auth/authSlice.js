a70d9b71b5cb10706c0642078e28a6fe914386a5
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-04-03 12:08:36 -0500

    set up frontend structure with react and redux

---
+++ frontend/src/redux/features/auth/authSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+});
+
+export const {} = authSlice.actions;
+export default authSlice.reducer;
