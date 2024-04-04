71467dd7f4f4d7c7ea90d133210f41edfb2177cc
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-04-04 16:35:05 -0500

    create authSlice

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -1,14 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const name = JSON.parse(localStorage.getItem("name"));
+
 const initialState = {
   isLoggedIn: false,
+  name: name ? name : "",
+  user: {
+    name: "",
+    email: "",
+    phone: "",
+    bio: "",
+    photo: "",
+  },
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    SET_LOGIN(state, action) {
+      state.isLoggedIn = action.payload;
+    },
+    SET_NAME(state, action) {
+      localStorage.setItem("name", JSON.stringify(action.payload));
+      state.name = action.payload;
+    },
+    SET_USER(state, action) {
+      const profile = action.payload;
+      state.name = profile.user.name;
+      state.email = profile.user.email;
+      state.phone = profile.user.phone;
+      state.bio = profile.user.bio;
+      state.photo = profile.user.photo;
+    },
+  },
 });
 
-export const {} = authSlice.actions;
+export const { SET_LOGIN, SET_NAME, SET_USER } = authSlice.actions;
 export default authSlice.reducer;
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+export const selectName = (state) => state.auth.name;
+export const selectUser = (state) => state.auth.user;
