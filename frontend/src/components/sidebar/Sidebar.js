6b33b19bf0098ad27bec17bafc4ae7548039b5f4
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-04-04 02:29:18 -0500

    create Dashboard components with route and add styling

---
+++ frontend/src/components/sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = ({ children }) => {
+  return (
+    <div className="layout">
+      <div className="sidebar">
+        <h2>Sidebar</h2>
+      </div>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ frontend/src/components/sidebar/Sidebaritem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebaritem = () => {
+  return <div>Sidebaritem</div>;
+};
+
+export default Sidebaritem;
