f4784d2c66feeeaac4b547eaf6fc05c73c479193
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2024-04-20 18:10:12 -0500

    add registerUser validation

--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./auth.module.css";
 import { TiUserAddOutline } from "react-icons/ti";
 import Card from "../../components/card/Card";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import registerUser, { validateEmail } from "../../services/authService";
+import { SET_LOGIN, SET_NAME } from "../../redux/features/auth/authSlice";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  password2: "",
+};
 
 const Register = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState(initialState);
+  const { name, email, password, password2 } = formData;
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const register = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      return toast.error("All fields are required.");
+    }
+    if (password.length < 6) {
+      return toast.error("Password must be at least six characters in length.");
+    }
+    if (!validateEmail(email)) {
+      return toast.error("Please enter a valid email.");
+    }
+    if (password !== password2) {
+      return toast.error("Passwords do not match.");
+    }
+
+    const userData = {
+      name,
+      email,
+      password,
+    };
+    setIsLoading(true);
+    try {
+      const data = await registerUser(userData);
+      // console.log(data);
+      await dispatch(SET_LOGIN(true));
+      await dispatch(SET_NAME(data.name));
+      navigate("/dashboard");
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className={`container ${styles.auth}`}>
       <Card>
@@ -14,25 +70,38 @@ const Register = () => {
           </div>
           <h2>Register</h2>
 
-          <form>
-            <input type="text" placeholder="Name" required name="name" />
+          <form onSubmit={register}>
+            <input
+              type="text"
+              placeholder="Name"
+              required
+              name="name"
+              value={name}
+              onChange={handleInputChange}
+            />
             <input
               type="email"
               placeholder="Email Address"
               required
               name="email"
+              value={email}
+              onChange={handleInputChange}
             />
             <input
               type="password"
               placeholder="Password"
               required
               name="password"
+              value={password}
+              onChange={handleInputChange}
             />
             <input
               type="password"
               placeholder="Confirm Password"
               required
-              name="password"
+              name="password2"
+              value={password2}
+              onChange={handleInputChange}
             />
             <button type="submit" className="--btn --btn-primary --btn-block">
               Login
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -4,6 +4,12 @@ import "react-toastify/dist/ReactToastify.css";
 
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
+export const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
 const registerUser = async (userData) => {
   try {
     const response = await axios.post(
